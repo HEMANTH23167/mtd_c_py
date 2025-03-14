e86e39322af493abfb615baf538c82d670b58502
HEMANTH B R <hemanthhrh23167@gmail.com>
2025-03-14 07:07:41 +0000

    day5

---
+++ p2.py
@@ -0,0 +1,6 @@
+def my_function(length,breadth):
+    pass
+length=int(input('enter the length of the rectangle:'))
+breadth=int(input('enter the breadth of the rectangle:'))
+area=length*breadth
+print(f'area of the rectangle={area}')
